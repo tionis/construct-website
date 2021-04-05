0ae13ef432df7ce06688fc8c374e54da73cc4ac5
tionis <tionis@tasadar.net>
2021-04-06 00:13:30 +0200

    fixed link to documentation site

--- src/routes/docs.svelte
+++ src/routes/docs.svelte
@@ -4,4 +4,4 @@
 
 <h1>Documentation</h1>
 
-<p>The documentation for the Construct Project can be found at the <a style="color:red" href="https://docs.tasadar.net">documentation website</a>.</p>
+<p>The documentation for the Construct Project can be found at the <a style="color:red" href="https://docs.construct.tasadar.net">documentation website</a>.</p>
